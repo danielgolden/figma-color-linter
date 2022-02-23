ece329d9c9e3798383b9d2ac8c01f53ee8c7fb12
danielgolden <danielgolden90@gmail.com>
2022-02-23 07:07:01 -0500

    feat: begint to build the UI

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,49 +1,109 @@
 import * as React from 'react';
-import { useState } from 'react';
-import _ from 'lodash'
+import {useState} from 'react';
+import _ from 'lodash';
 import '../styles/ui.css';
 
 declare function require(path: string): any;
 
 const App = ({}) => {
     const [fileColorStats, setFileColorStats] = useState([]);
-    const textbox = React.useRef<HTMLInputElement>(undefined);
-
-    const countRef = React.useCallback((element: HTMLInputElement) => {
-        if (element) element.value = '5';
-        textbox.current = element;
-    }, []);
-
-    const onCreate = () => {
-        const count = parseInt(textbox.current.value, 10);
-        parent.postMessage({pluginMessage: {type: 'create-rectangles', count}}, '*');
-    };
-
-    const onCancel = () => {
-        parent.postMessage({pluginMessage: {type: 'cancel'}}, '*');
-    };
+    const [selectionColorStats, setSelectionColorStats] = useState({});
 
     React.useEffect(() => {
+        parent.postMessage({pluginMessage: {type: 'request-selection'}}, '*');
+
         // This is how we read messages sent from the plugin controller
         window.onmessage = (event) => {
             const {type, message} = event.data.pluginMessage;
-            if (type === 'file-color-stats') {
-                setFileColorStats(message)
+
+            if (type === 'color-stats') {
+                setSelectionColorStats(message);
             }
+
+            // if (type === "selection-change") {
+            //   // Filter out text layers that have no suggestions
+            //   setSelectionColorStats(message)
+            // }
         };
     }, []);
 
+    const handleColorTileClick = (layerId: string) => {
+        parent.postMessage(
+            {
+                pluginMessage: {
+                    type: 'select-layer',
+                    layerId: layerId,
+                },
+            },
+            '*'
+        );
+    };
+
+    const renderColorIssues = () => {
+        const colorsWithIssues = selectionColorStats?.colorsNotUsingOneCoreColorStyle;
+
+        const truncateLayerName = (layerName: string): string => {
+            if (layerName.length > 30) {
+                return layerName.substring(0, 27) + '...';
+            }
+
+            return layerName;
+        };
+
+        if (colorsWithIssues) {
+            return colorsWithIssues.map((color, index) => {
+                const {
+                    colorId,
+                    layerId,
+                    layerName,
+                    layerType,
+                    colorStyleId,
+                    hasColorStyle,
+                    visible,
+                    colorType,
+                    colorInHex,
+                } = color;
+
+                return (
+                    <li className="color-tile-container" key={index} onClick={() => handleColorTileClick(layerId)}>
+                        <div className="color-tile-header">
+                            <h4>Missing One Core Color style</h4>
+                            <button>...</button>
+                        </div>
+
+                        <ul className="color-tile-meta-list">
+                            <li className="color-tile-meta-list-item color-tile-meta-color">
+                                <span style={{backgroundColor: colorInHex}} className="color-tile-color-sample"></span>
+                                {colorInHex}
+                            </li>
+                            <li className="color-tile-meta-list-item color-tile-meta-layer-name">
+                                Layer: {truncateLayerName(layerName)}
+                            </li>
+                            <li className="color-tile-meta-list-item">Type: {colorType}</li>
+                        </ul>
+
+                        <article className="suggested-color-styles-container">
+                            <div className="suggested-color-style-header">
+                                <h5>Suggested color styles</h5>
+                                <button className="suggested-color-style-help">?</button>
+                            </div>
+
+                            <ul className="suggested-color-style-list">
+                                <li className="suggested-color-style-list-itme">Text / Color / Token / Name</li>
+                                <li className="suggested-color-style-list-itme">Text / Color / Token / Name</li>
+                                <li className="suggested-color-style-list-itme">Text / Color / Token / Name</li>
+                                <li className="suggested-color-style-list-itme">Text / Color / Token / Name</li>
+                            </ul>
+                        </article>
+                    </li>
+                );
+            });
+        }
+    };
+
     return (
         <div>
-            <img src={require('../assets/logo.svg')} />
-            <h2>Rectangle Creator</h2>
-            <p>
-                Count: <input ref={countRef} />
-            </p>
-            <button id="create" onClick={onCreate}>
-                Create
-            </button>
-            <button onClick={onCancel}>Cancel</button>
+            <ul className="color-tiles-container">{renderColorIssues()}</ul>
         </div>
     );
 };
